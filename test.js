8575ed3f67e4db6f4fb263877b17844c5f2915ab
Jacob Quant <jacobq@gmail.com>
2018-03-12 18:14:22 -0500

    Refactor test suite exact/range URL generation

--- test.js
+++ test.js
@@ -48,18 +48,26 @@ const rangeRefs = [
 
 const exactRefs = [
   '#master',
-  '#jj-abrams',
+  '#something-with-hyphens',
   '#9bfce8f957a80c93d5c6365377d07a59034c6482',
   '#semver:1.0.0'
 ]
 
+const exactURLs = baseURLs.reduce((urls, baseURL) => {
+  return urls.concat(exactRefs.map(ref => `${baseURL}${ref}`))
+}, [])
+
+const rangeURLs = baseURLs.reduce((urls, baseURL) => {
+  return urls.concat(rangeRefs.map(ref => `${baseURL}${ref}`))
+}, [])
+
 test('exact commitish strings -> true', function (t) {
-  baseURLs.forEach((baseURL) => {
-    exactRefs.forEach((ref) => {
-      const s = `${baseURL}${ref}`
-      t.ok(exact(s), `${s} is exact`)
-    })
-  })
+  exactURLs.forEach(url => t.ok(exact(url), url))
+  t.end()
+})
+
+test('range commitish strings -> true', function (t) {
+  rangeURLs.forEach(url => t.notOk(exact(url), url))
   t.end()
 })
 
@@ -70,15 +78,5 @@ test('Miscellaneous exact -> true', function (t) {
   const localFile = '../some/file.js'
   t.ok(exact(localFile), `Local file ('${localFile}') should be considered exact`)
 
-  t.end()
-})
-
-test('range commit-ish strings -> false', function (t) {
-  baseURLs.forEach((baseURL) => {
-    rangeRefs.forEach((ref) => {
-      const s = `${baseURL}${ref}`
-      t.notOk(exact(s), `${s} refers to a range`)
-    })
-  })
   t.end()
 })
