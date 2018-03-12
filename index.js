1fbd492e276962bd1b6287d5c2188bc4f5840892
Jacob Quant <jacobq@gmail.com>
2018-03-12 15:10:54 -0500

    Handle commitish strings

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
-var regex = /\^|~|<|>|\|/
+// Consider it a range if we see any of the symbols ^~<>| or a digit followed by ".x"
+var regex = /[\^~<>|]|\d\.x/
 
 module.exports = function exactVersion (version) {
   return !regex.test(version)
--- test.js
+++ test.js
@@ -1,15 +1,61 @@
 'use strict'
 
-var test = require('tape')
-var exact = require('./')
+const test = require('tape')
+const exact = require('./')
 
-test(function (t) {
+test('exact versions -> true', function (t) {
   t.ok(exact('4.0.0'))
   t.ok(exact('=4.0.0'))
+  t.end()
+})
+
+test('range versions -> false', function (t) {
   t.notOk(exact('~4.0.0'))
   t.notOk(exact('^4.0.0'))
+  t.notOk(exact('1.2.x'))
   t.notOk(exact('>= 4.0.0'))
   t.notOk(exact('<= 4.0.0'))
   t.notOk(exact('4.0.0 || 4.0.1'))
+  t.end()
+})
+
+const baseURLs = [
+  'git@github.com:bendrucker/exact-version.git#master',
+  'github.com:bendrucker/exact-version.git',
+  'bendrucker/exact-version.git',
+  'https://github.com:bendrucker/exact-version.git',
+  'git+ssh://git@github.com:substack/tape.git'
+]
+
+const rangeRefs = [
+  '#semver:^5.0',
+  '#semver:~1.2.0',
+  '#semver:2.x'
+]
+
+const exactRefs = [
+  '#master',
+  '#jj-abrams',
+  '#9bfce8f957a80c93d5c6365377d07a59034c6482',
+  '#semver:1.0.0'
+]
+
+test('exact commitish strings -> true', function (t) {
+  baseURLs.forEach((baseURL) => {
+    exactRefs.forEach((ref) => {
+      const s = `${baseURL}${ref}`
+      t.ok(exact(s), `${s} is exact`)
+    })
+  })
+  t.end()
+})
+
+test('range commit-ish strings -> false', function (t) {
+  baseURLs.forEach((baseURL) => {
+    rangeRefs.forEach((ref) => {
+      const s = `${baseURL}${ref}`
+      t.notOk(exact(s), `${s} refers to a range`)
+    })
+  })
   t.end()
 })
